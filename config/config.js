65596c6b0728d365b8b361dec4e027a9f845153f
Florian Jenett <fjenett@gmail.com>
2013-10-07 16:23:46 +0200

    added example config

---
+++ config/config.js
@@ -0,0 +1,20 @@
+
+/* NOTE: this configuration file will override the 
+default one in app-base/config/config.js */
+
+module.exports = {
+
+	debug : 	false,
+	islocal : 	true,
+
+	host : 		'localhost:3331',
+	apiHost : 	'localhost:5555',
+	adminHost : 'localhost:5556',
+ 	baseUrl : 	'',
+
+	cloudFront : {
+		baseUrl :  '/dh/app',
+		fileHost : 'd35vpnmjdsiejq.cloudfront.net',
+		streamer : 's12vlv7g59gljg.cloudfront.net'
+	}
+};
